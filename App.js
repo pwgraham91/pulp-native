841689961b1e6b511d6abcf029cea02b299479f0
Peter Graham <pwgraham91@gmail.com>
2018-08-22 23:16:40 -0500

    name

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Image, StyleSheet, Text, TextInput, View} from 'react-native';
+import InputTester from "./components/InputTester";
 
 export default class App extends React.Component {
     render() {
+        let pic = {
+            uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+        }
         return (
             <View style={styles.container}>
-                <Text>Open up App.js to start working on your app!</Text>
-                <Text>Changes you make will automatically reload.</Text>
-                <Text>Shake your phone to open the developer menu.</Text>
+                <Image source={pic} style={{
+                    width: 193,
+                    height: 110
+                }} />
+                <InputTester/>
             </View>
         );
     }
