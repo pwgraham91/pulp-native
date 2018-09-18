f8a94b9acaf318271de9087664b74f98a9585607
Peter Graham <pwgraham91@gmail.com>
2018-09-18 16:25:33 -0500

    no console

--- components/InputViewComponent.js
+++ components/InputViewComponent.js
@@ -35,7 +35,6 @@ class InputViewComponent extends Component {
     }
 
     updateEmail(text) {
-        console.log('user', this.props.user)
         this.setState({
             ...this.state,
             email: text
