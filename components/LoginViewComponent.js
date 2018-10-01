980d475d70bab7d8c1d9ed65fde2369e430eccc8
Peter Graham <pwgraham91@gmail.com>
2018-09-30 23:31:03 -0500

    get logging out

--- components/LoginViewComponent.js
+++ components/LoginViewComponent.js
@@ -17,21 +17,8 @@ class LoginViewComponent extends Component {
     this.updateEmail = this.updateEmail.bind(this);
     this.updatePassword = this.updatePassword.bind(this);
     this.authenticate = this.authenticate.bind(this);
-    this.incrementCounter = this.incrementCounter.bind(this);
-    this.storeData = this.storeData.bind(this);
-    this.verify = this.verify.bind(this);
   }
 
-  verify() {
-    this.props.user.axios
-      .get('/protected')
-      .then(responseJson => {
-        console.log('response', responseJson);
-      })
-      .catch(error => {
-        console.log('error', error);
-      });
-  }
   authenticate() {
     this.props.user.axios
       .post('/login', JSON.stringify(this.state))
@@ -57,41 +44,6 @@ class LoginViewComponent extends Component {
     });
   }
 
-  incrementCounter() {
-    this.props.incrementCounterAction();
-    // this._retrieveData();
-  }
-
-  storeData() {
-    this._storeData();
-  }
-
-  _retrieveData = async () => {
-    try {
-      const value = await AsyncStorage.getItem('@UserStore:incrementer');
-      const parsed = JSON.parse(value);
-      console.log('valuing', parsed);
-      console.log('valuing2', parsed.increment);
-    } catch (error) {
-      console.log('erroring', error);
-      // Error retrieving data
-    }
-  };
-  _storeData = async () => {
-    try {
-      console.log('storing');
-      // todo move this code to the reducer and save the access token to local storage then get it on the way in for default
-      await AsyncStorage.setItem(
-        '@UserStore:incrementer',
-        JSON.stringify({
-          increment: 2,
-        })
-      );
-    } catch (error) {
-      console.log('error store', error);
-      // Error saving data
-    }
-  };
   render() {
     return (
       <View style={styles.container}>
@@ -111,21 +63,6 @@ class LoginViewComponent extends Component {
           style={styles.buttonStyle}
           title="Log In"
           onPress={this.authenticate}
-        />
-        <Button
-          style={styles.buttonStyle}
-          title="Increment"
-          onPress={this.incrementCounter}
-        />
-        <Button
-          style={styles.buttonStyle}
-          title="store"
-          onPress={this.storeData}
-        />
-        <Button
-          style={styles.buttonStyle}
-          title="verify"
-          onPress={this.verify}
         />
       </View>
     );
