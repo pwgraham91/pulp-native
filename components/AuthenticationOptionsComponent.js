919a2de7ba5a8d63bd21aa91768eee7fbfedda78
Peter Graham <pwgraham91@gmail.com>
2018-10-04 00:07:53 -0500

    working nice looking button

--- components/AuthenticationOptionsComponent.js
+++ components/AuthenticationOptionsComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
+import { Button } from 'native-base';
 
 export default class AuthenticationOptionsComponent extends Component {
   constructor(props) {
@@ -10,13 +11,15 @@ export default class AuthenticationOptionsComponent extends Component {
     const { navigate } = this.props.navigation;
 
     return (
-      <View style={styles.container}>
-        <TouchableOpacity
+      <View>
+        <Button
+          bordered
+          light
           style={styles.buttonStyle}
           onPress={() => navigate('LoginView')}
         >
           <Text style={styles.textStyle}>Login</Text>
-        </TouchableOpacity>
+        </Button>
       </View>
     );
   }
@@ -30,13 +33,9 @@ const styles = StyleSheet.create({
   textStyle: {
     fontSize: 20,
     color: '#ffffff',
-    textAlign: 'center',
   },
   buttonStyle: {
-    padding: 10,
-    backgroundColor: '#202646',
-    borderRadius: 5,
     width: 200,
-    margin: 10,
+    justifyContent: 'center',
   },
 });
